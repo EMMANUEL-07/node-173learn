309e9828a470fa8753d2fb04094c0e5be916ab26
EMMANUEL <emmanueloyekan33@gmail.com>
2020-08-25 20:21:03 +0100

    ADDED NEW FEATURE TO forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,28 +8,29 @@ const forecast = (lonn, latt, callback) => {
         lat: latt.toString(),
         lon: lonn.toString(),
         unit_system: 'si',
-        fields: 'temp,humidity,sunset',
+        fields: 'temp,humidity,sunset,sunrise',
         apikey: 'GcBLkvgkSM7v5Zvu4aK6OX9yCqHyOaxU'
       },
       json: true 
     };
   
     request(options, (error, {body}) => {
-        if (error) {
-          callback('Unable to connect to weather service', undefined)
-        }else if(body.message){
-        callback(body.message, undefined)
-        }else {
-            temp = "The temperature currently is "+ body.temp.value + " " + body.temp.units;
-            humid = "Since we are in the evening, the humidity is "+ body.humidity.value + " %";
-            sset ="The sun is to set by "+ body.sunset.value;
-            rem = "Thank you for patronizing us";
-    
-            data = temp + ". " + humid + ". " + sset + ". " + rem + ". "
-    
-            callback(undefined, data)
-    
-        }
+      if (error) {
+        callback('Unable to connect to weather service', undefined)
+      }else if(body.message){
+      callback(body.message, undefined)
+      }else {
+        temp = "The temperature currently is "+ body.temp.value + " " + body.temp.units;
+        humid = "Since we are in the evening, the humidity is "+ body.humidity.value + " %";
+        sset ="The sun is to set by "+ body.sunset.value;
+        srise = "The sun rose by "+ body.sunrise.value;
+        rem = "Thank you for patronizing us";
+
+        data = temp + ". " + humid + ". " + srise + ". " + sset + ". " + rem + ". "
+
+        callback(undefined, data)
+  
+      }
 
     });
 }
